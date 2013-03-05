c956f47a466f93bfb6758d3159b38fc5c78cc529
Heikki Lappalainen <heikkiklappalainen@gmail.com>
2013-03-05 22:51:52 +0200

    Create ape_man.dart

---
+++ ape_man.dart
@@ -0,0 +1,43 @@
+//  simple extending classes example..
+//  developing this still further...
+
+// This class exists simply for others to extend
+class Ape {
+  String apeFood = "Eating juicy banana...  ";
+  String apeJob  = "Scratching my back..... ";
+  bool wannaMoreBananas = true;
+}
+
+//  Human class learns something from apes and gives it forward..
+class Human extends Ape {
+  String humanThinking = "What do others think about me..?  ";
+ }
+
+
+// this class will have an instance AND it uses properties
+// inherited from other classes
+class Programmer extends Human {
+  String progJob = "Solving problems and more problems...";
+}
+
+void main() {
+  print("Hei, Apina!!  Terve, Ihminen!  Moikka, ohjelmoija!!  ");
+
+// here Programmer class will be instantiated
+  var p = new Programmer();
+
+// Class programmer, p. !! uses some ape skills wich it inherited..
+  print(p.apeFood);
+  print(p.apeJob);
+
+ // and Programmer class uses also some human properties.. 
+  print(p.humanThinking);
+  if (p.wannaMoreBananas) print('I Wanna More Bananas !!');
+
+  // this programmer do not have many own skills yet,
+  // but we can print out, what he/she is thinking
+  print(' You asking what I am doing?? ');
+  print(p.progJob);
+
+  print("===   End of program   ===");
+}

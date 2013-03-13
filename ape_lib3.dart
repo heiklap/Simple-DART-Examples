f0e83497c602de972ea9f995d031dd57c593fcfd
Heikki Lappalainen <heikkiklappalainen@gmail.com>
2013-03-13 17:45:17 +0200

    Update ape_lib3.dart

--- ape_lib3.dart
+++ ape_lib3.dart
@@ -1,13 +1,2 @@
-
-part of ape_man3;
-
-
-
-class Ape {
-  String  dailyFood    =  "Eating juicy banana...  ";
-  num     mealsADay    =  4;
-  String  dailyJob     =  "Scratching my head..... ";
-  bool    isHungry     =   true;
-  String  apeThink   =   "hmmmmmm  ";
-  String  apeMessage = "We are now in ape classs  ";
-}
+Wrong name conventionsa.
+i must think it again,, must delete this..
